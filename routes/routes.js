2187c5ed023b2dd35320b1f06e0d08199cdc123c
Prima Aulia Gusta <primaulia@gmail.com>
2016-10-31 16:46:51 +0800

    routes updates

--- routes/routes.js
+++ routes/routes.js
@@ -1,1 +1,10 @@
-cd ..
+var express = require('express')
+var router = express.Router()
+
+var userController = require('../controllers/userController')
+
+router.route('/')
+      .get(userController.getSignup)
+      .post(userController.postSignup)
+
+module.exports = router
